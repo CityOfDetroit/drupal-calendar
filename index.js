d4143646028e0cec2fab574f0c5bbef1ca91f197
Edgar Montes <jedgar1mx@gmail.com>
2020-06-09 18:34:20 -0400

    removing test data.

--- index.js
+++ index.js
@@ -3,31 +3,6 @@ import { Calendar } from '@fullcalendar/core';
 import '@fullcalendar/core/main.css';
 import Controller from './controller.class';
 import './sass/styles.scss';
-
-const drupalSettings = {
-    intranet : {
-        intranetJs : {
-            eventItems: {
-                "51": {
-                    id: '51',
-                    name: 'Web Team Meeting',
-                    start: '2019-08-13',
-                    departmentID: '6',
-                    departmentName: 'Web Team',
-                    type: 'Meeting'
-                },
-                "52": {
-                  id: '52',
-                  name: 'City Council Session',
-                  start: '2019-08-12',
-                  departmentID: '3',
-                  departmentName: 'City Council',
-                  type: 'Event'
-              }
-            }
-        }
-    }
-}
 let data;
 try {
     data = drupalSettings.intranet.intranetJs.eventItems
